6c743aa0092b4f93fba69e032c9565a692397e81
Pedro Navarrete Hurtado <navhpedro@gmail.com>
2018-05-10 17:41:40 -0500

    Organizing the Repo

--- C++/sphere.h
+++ C++/sphere.h
@@ -1,17 +1,24 @@
+/* Spheere Class Definition*/
 class Sphere {
 public:
+  //Constructors
   Sphere(double R);
   Sphere();
+  //Destructor
   ~Sphere();
 
-  void SetRadius(double R);
+  //Mutator
+  void set_radius(double R);
+
+  //Accesors
   double Radius();
   double Diameter();
   double Circumference();
   double Area();
   double Volume();
-
   void DisplayInfo();
+
 private:
+  //Member Variables
   double TheRadius;
 };
